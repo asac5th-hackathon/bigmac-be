45585b6876dbeb73a2b37dfb840d4e65d2557a60
ptokos <jiseokchoij@gmail.com>
2024-07-04 01:32:42 +0900

    [Feat] 가격 등록하는 API

--- src/main/java/com/nct9/bigmac/domain/product/repository/ProductRepository.java
+++ src/main/java/com/nct9/bigmac/domain/product/repository/ProductRepository.java
@@ -2,11 +2,10 @@ package com.nct9.bigmac.domain.product.repository;
 
 import com.nct9.bigmac.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByStoreIdAndCategoryId(Long storeId, Long categoryId);
-    List<Product> findByStoreId(Long storeId);
-}
 
+
+}
