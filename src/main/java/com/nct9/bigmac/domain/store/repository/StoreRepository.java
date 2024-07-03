9e5862cf928b3391684d3e916417878368fcbfde
Jiseok <90011647+ptokos@users.noreply.github.com>
2024-07-04 01:43:53 +0900

    Revert "Feature/price 가격 등록하는 API"

--- src/main/java/com/nct9/bigmac/domain/store/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nct9.bigmac.domain.store.repository;
-
-
-import com.nct9.bigmac.domain.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
