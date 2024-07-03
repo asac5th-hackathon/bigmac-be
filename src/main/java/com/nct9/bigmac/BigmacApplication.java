ac677d46623df71d36cec6094ea3d8ff9ad6eeae
ji-mango <susie000301@naver.com>
2024-07-03 19:36:13 +0900

    first commit

---
+++ src/main/java/com/nct9/bigmac/BigmacApplication.java
@@ -0,0 +1,13 @@
+package com.nct9.bigmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigmacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigmacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nct9/bigmac/BigmacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nct9.bigmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigmacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
